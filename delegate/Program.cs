d72073b2bf903810755f60729d6e194988fa732f
Ilya11111 <62544436+Ilya11111@users.noreply.github.com>
2021-07-16 17:52:41 +0300

    Delegate
    C#

---
+++ delegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace delegate1
+{
+    class Program
+{
+        private static void HelloMessage() 
+        {
+            Console.WriteLine("Hello");
+        }
+        private static void ByeMessage()
+        {
+            Console.WriteLine("Bye");
+        }
+        private static void WorkMessage(DayOfWeek day)
+        {
+            Console.WriteLine("Work "+ day);
+        }
+        delegate void MyFirstDelegate();
+        delegate void MySecondDelegate(DayOfWeek day);
+        static void Main(string[] args)
+    {
+            MyFirstDelegate del;
+            MySecondDelegate del2;
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Monday)
+            {
+                del = HelloMessage;
+                del();
+            }
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                del = ByeMessage;
+                del();
+            }
+            if((DateTime.Now.DayOfWeek != DayOfWeek.Monday) & (DateTime.Now.DayOfWeek != DayOfWeek.Sunday))
+            {
+                del2 = WorkMessage;
+                del2(DateTime.Now.DayOfWeek);
+            }
+              
+            
+    }
+}
+}
+
